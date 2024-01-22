043663859073eb0a1cb844b0667d27048cfd0656
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-22 12:15:05 +0900

    main.js 修正  close #3

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
 'use strict';
 {
-    function render(quiz) {
-        const main = document.querySelector('main');
-        const section = document.createElement('section');
-        const h2 = document.createElement('h2');
-        h2.textContent = quiz[0];
-
-        const ul = document.createElement('ul');
-        for( let i = 1; i <= 3; i++ ) {
-            const li0 = document.createElement('li');
-            li0.textContent = quiz[i];
-            li0.addEventListener('click',()=> {
-                if ( quiz[4] === i-1 ) {
-                    li0.classList.add('correct');
-                } else {
-                    li0.classList.add('wrong');
-                }
-            });
-            ul.appendChild(li0);
-            section.appendChild(h2);
-            section.appendChild(ul);
-            main.appendChild(section);
-            }
-
-        
-    }
-    const quizzes = [
-        ['1の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 0],
-        ['2の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 1],
-        ['3の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 2],
-        ['4の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 2],
-        ['5の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 3],
-        ['6の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 2],
-        ['7の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 1],
-        ['8の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 3],
-        ['9の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 1],
-        ['10の正解は？', '選択肢 A', '選択肢 B', '選択肢 C',2],
-    ]
-
-    quizzes.forEach((value)=> {
-        render(value);
+    const quizObj = {
+        title: '三択クイズ',
+        quizzes: [
+            ['Q.01', '','選択肢 A', '選択肢 B', '選択肢 C', 0],
+            ['Q.02', '','選択肢 A', '選択肢 B', '選択肢 C', 1],
+            ['Q.03', '','選択肢 A', '選択肢 B', '選択肢 C', 2],
+            ['Q.04', '','選択肢 A', '選択肢 B', '選択肢 C', 2],
+            ['Q.05', '','選択肢 A', '選択肢 B', '選択肢 C', 3],
+            ['Q.06', '','選択肢 A', '選択肢 B', '選択肢 C', 2],
+            ['Q.07', '','選択肢 A', '選択肢 B', '選択肢 C', 1],
+            ['Q.08', '','選択肢 A', '選択肢 B', '選択肢 C', 3],
+            ['Q.09', '','選択肢 A', '選択肢 B', '選択肢 C', 1],
+            ['Q.10', '','選択肢 A', '選択肢 B', '選択肢 C',2],
+        ],
+        render: function(quiz) {
+            const main = document.querySelector('main');
+            const section = document.createElement('section');
+            const h2 = document.createElement('h2');
+            const p =document.createElement('p');
+            h2.textContent = quiz[0];
+            p.textContent = quiz[1];
+            const ul = document.createElement('ul');
+            for( let i = 2; i <= 4; i++ ) {
+                const li0 = document.createElement('li');
+                li0.textContent = quiz[i];
+                li0.addEventListener('click',()=> {
+                    if ( quiz[4] === i-1 ) {
+                        li0.classList.add('correct');
+                    } else {
+                        li0.classList.add('wrong');
+                    }
+                });
+                ul.appendChild(li0);
+                section.appendChild(h2);
+                section.appendChild(p);
+                section.appendChild(ul);
+                main.appendChild(section);
+                }        
+        }
+        }
+    
+    quizObj.quizzes.forEach((value)=> {
+        quizObj.render(value);
     });
+
 }
