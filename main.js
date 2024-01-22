15304473856531d12a30432314107807c47c6683
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-22 10:53:21 +0900

    ループ処理を使ってコードの修正をしました

--- main.js
+++ main.js
@@ -3,51 +3,26 @@
     function render(quiz) {
         const main = document.querySelector('main');
         const section = document.createElement('section');
-        const ul = document.createElement('ul');
-
-        const li0 = document.createElement('li');
-        li0.addEventListener('click',()=> {
-            if ( quiz[4] === 0 ) {
-                li0.classList.add('correct');
-            } else {
-                li0.classList.add('wrong');
-            }
-        });
-        li0.textContent = quiz[1];
-
-        const li1 = document.createElement('li');
-        li1.addEventListener('click',()=> {
-            if ( quiz[4] === 1 ) {
-                li1.classList.add('correct');
-            } else {
-                li1.classList.add('wrong');
-            }
-        });
-        li1.textContent = quiz[2];
-
-       const li2 = document.createElement('li');
-        li2.addEventListener('click',()=> {
-            if ( quiz[4] === 2 ) {
-                li2.classList.add('correct');
-            } else {
-                li2.classList.add('wrong');
-            }
-        });
-        li2.textContent = quiz[3];
-
-
         const h2 = document.createElement('h2');
         h2.textContent = quiz[0];
-        ul.appendChild(li0);
-        ul.appendChild(li1);
-        ul.appendChild(li2);
-        
-        
-                
-        section.appendChild(h2);
-        section.appendChild(ul);
-        main.appendChild(section);
-        
+
+        const ul = document.createElement('ul');
+        for( let i = 1; i <= 3; i++ ) {
+            const li0 = document.createElement('li');
+            li0.textContent = quiz[i];
+            li0.addEventListener('click',()=> {
+                if ( quiz[4] === i-1 ) {
+                    li0.classList.add('correct');
+                } else {
+                    li0.classList.add('wrong');
+                }
+            });
+            ul.appendChild(li0);
+            section.appendChild(h2);
+            section.appendChild(ul);
+            main.appendChild(section);
+            }
+
         
     }
     const quizzes = [
